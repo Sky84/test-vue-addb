116fbc17fc16d8edbe84845754f3c25b4e2cab09
Salupo Etienne <etienne.salupocorvo@gmail.com>
2020-04-13 16:06:42 +0200

    [FEAT] header total amount in cart + router working

---
+++ src/components/RouterLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <a @click="navigate" :href="to">{{label}}</a>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class RouterLink extends Vue {
+  @Prop(String) private to!: string;
+  @Prop(String) private label!: string;
+
+  private navigate(evt: any) {
+    evt.preventDefault();
+    window.history.pushState(null, '', this.to);
+    this.$root.$emit('navigate');
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
